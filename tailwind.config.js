7302ea92c0359dec740fc8394a68c79e9898efa6
hulkoba <jacoba@neighbourhood.ie>
2023-09-12 20:19:37 +0200

    fix: tailwind config view movement

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./resources/views/**/*.edge'],
+  content: ['./app/views/**/*.edge'],
   theme: {
     fontFamily: {
       // sans: ['Graphik', 'sans-serif'],
